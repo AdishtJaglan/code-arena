4a330aa17264cb9cca20473bedda6cb9dc69dcd1
AdishtJaglan <adisht7524@gmail.com>
2024-11-25 23:06:41 +0530

    setup cloudinary

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import { ENV } from "../config/env-config";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: ENV.CLOUDINARY_CLOUD_NAME,
+  api_key: ENV.CLOUDINARY_API_KEY,
+  api_secret: ENV.CLOUDINARY_API_SECRET,
+});
+
+const uploadImageToCloudinary = async (filePath) => {
+  try {
+    if (!filePath) return null;
+
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File uploading to cloudinary: " + response);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(filePath);
+  }
+};
+
+export default uploadImageToCloudinary;
